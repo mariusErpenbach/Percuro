2682e0a94f118a5f96b137950bd5dc4dc34f4881
Hawty1 <marius_erpenbach@web.de>
2025-04-22 21:12:36 +0200

    EnterpriseView new

---
+++ ViewModels/EnterpriseViewModel.cs
@@ -0,0 +1,14 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Avalonia.Threading;
+
+namespace Percuro.ViewModels;
+public partial class EnterpriseViewModel : ViewModelBase
+{
+    public EnterpriseViewModel()
+    {
+    
+    }
+
+
+}
