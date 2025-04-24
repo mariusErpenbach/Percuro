0bb9b83b5303cb217745566848b86ce5e9770229
Hawty1 <marius_erpenbach@web.de>
2025-04-24 04:56:56 +0200

    AnAnalyticsViews

--- obj/Debug/net9.0/Percuro.AssemblyInfo.cs
+++ obj/Debug/net9.0/Percuro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Percuro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9dc45093ddaf30e56cd88db4e3f43ef10be4856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e65c41643b096ff2428bb04fa53e4b94992a1df8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Percuro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Percuro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
